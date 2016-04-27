f4a0b0868e4f1dee90a4715f4b8cb951a5c3e515
RedEnergy <forwot163@gmail.com>
2016-04-27 21:35:58 +0400

    Optimized tick tracker updater

--- src/main/java/ru/redenergy/party/handler/TrackerTickUpdater.java
+++ src/main/java/ru/redenergy/party/handler/TrackerTickUpdater.java
@@ -11,7 +11,9 @@ import net.minecraftforge.fml.common.gameevent.TickEvent;
 import ru.redenergy.party.PartyController;
 import ru.redenergy.party.model.PartyInfo;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -29,15 +31,17 @@ public class TrackerTickUpdater {
         MinecraftServer server = FMLCommonHandler.instance().getMinecraftServerInstance();
         EntityTracker tracker = server.worldServers[0].getEntityTracker();
         for(PartyInfo party: controller.getParties()){
-            List<EntityPlayerMP> players = namesToPlayers(party.getAllPlayers(), server);
+            List<EntityPlayerMP> players = namesToPlayers(party.getAllUsers(), server);
             for(EntityPlayerMP player: players) {
+                List<EntityPlayerMP> tracked = new ArrayList<>(players);
+                tracked.remove(player);
                 EntityTrackerEntry entry = tracker.trackedEntityHashTable.lookup(player.getEntityId());
-                entry.trackingPlayers.addAll(players);
+                entry.trackingPlayers.addAll(tracked);
             }
         }
     }
 
     private List<EntityPlayerMP> namesToPlayers(Set<String> names, MinecraftServer server){
-        return names.stream().map(server.getPlayerList()::getPlayerByUsername).collect(Collectors.toList());
+        return names.stream().map(server.getPlayerList()::getPlayerByUsername).filter(Objects::nonNull).collect(Collectors.toList());
     }
 }
